f17225b6e9d7b99595d8736326eb490d88bd9c90
Maderlaine1993 <tu@email.com>
2023-10-21 10:54:16 -0600

    Se agrega ruta para buscar

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::get('get-empleados', [\App\Http\Controllers\EmpleadoController::class, 'g
 Route::put('save-empleados', [\App\Http\Controllers\EmpleadoController::class, 'save'])->name('api-save');
 Route::delete('delete-empleados/{id}', [\App\Http\Controllers\EmpleadoController::class, 'deleteEmpleado'])->name('api-deleteEmpleado');
 Route::post('edit-empleados/{id}', [\App\Http\Controllers\EmpleadoController::class, 'editEmpleado'])->name('api-editEmpleado');
+Route::get('get-empleados/{id}', [\App\Http\Controllers\EmpleadoController::class, 'getEmpleado'])->name('getEmpleado');
